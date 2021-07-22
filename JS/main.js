2eac47077be1491e759286875c4dff82b2fba56d
brunomorais2621 <brunomorais2621@gmail.com>
2021-07-22 14:56:23 -0300

    primeiro commit

---
+++ JS/main.js
@@ -0,0 +1,59 @@
+
+function botao(){                                                     //cria um botão clicável na página
+alert("Você é metaleiro mesmo!")
+}
+
+
+
+/*function soma(n1, n2){                                              //No JS também podemos trabalhar com funções
+    return n1 + n2;
+}
+ alert(soma(5, 10)); 
+ */
+
+
+/*var d = new Date()                                                  //Somente para exibir data
+alert (d.getHours());
+*/
+
+
+/*var count = 0;
+while (count <= 5){                                                  //Neste exemplo count vai contar de 0 somando mais 1 já que usei count++ até o limite 5
+    console.log(count);
+    count++
+}
+
+*/
+
+/*var idade = prompt("Qual a a sua idade?")                          //Prompt cria um popup de pergunta
+if (idade >=18){                                                     //If é a funcão condicional SE
+alert("pode escutar Steel Panther");
+}
+else {
+    alert("não pode escutar Stell Panther")
+}
+*/
+
+//var banda = {nome:"Mayhem", cor:"vermelha"}
+//console.log(banda);
+//alert(banda.nome);                                                 //No alerta da página vai me retornar o nome Mayhem
+
+
+//var lista =["iron", "judas", "Saxon"];
+//lista.push("Sabbath");
+//console.log(lista);
+//console.log(lista.reverse());                                     //Mostra a lista de últimi para o primeiro no console
+//console.log(lista);                                               //Comando push adiciona um item e o pop retira
+//console.log(lista.length);                                        //Com o length aparace quantos itens tenho na lista 
+//alert(lista[1]);                                                  //Neste caso o número 1 representa a posição Judas na lista
+
+//var nome = "Visitante"
+//alert("Bem vindo " + nome);                                       //Comando alert cria um popup de alerta, neste caso criei uma variável nome e com o "+" se concatena com a string "Bem vindo"
+//var frase = "Iron Maiden é a melhor banda do mundo";
+//console.log(frase.replace("Iron Maiden", "Metallica"));           //O console.log irá mostrar o resultado diretamente no console do navegador
+//alert(frase)
+
+
+
+
+
